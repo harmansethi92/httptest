a43feaae72bcaaa2c1bfbb3caac9855e6ca11103
harmansethi92 <harmansethi92@gmail.com>
2017-04-29 00:34:38 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import BaseHTTPServer
+from random import randint
+
+HOST_NAME = ''
+PORT_NUMBER = int(sys.argv[1])
+
+class HiHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        time.sleep(randint(0,10))
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("Hi!\n")
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), HiHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
